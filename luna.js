5eba8d4c173640409204e4474d16cb243ba2103b
PocketSFabuser <kolos.comm2@gmail.com>
2025-01-09 00:05:31 +0300

    Update luna.js
    bug fix

--- luna.js
+++ luna.js
@@ -3,9 +3,17 @@ const btn = document.getElementById('more');
 window.onload = () => {
 
 btn.addEventListener('click', () => {
-    console.err ('govnoo');
+    console.error ('govnoo');
     window.open ('luna-more.html');
     window.close();
 } );
 
+}
+
+boolean sosal = true;
+
+if ( sosal != false && 2 + 2 = 4 ) {
+    console.log ('Z");
+} else {
+    console.log ('kak ebat?!");
 }
